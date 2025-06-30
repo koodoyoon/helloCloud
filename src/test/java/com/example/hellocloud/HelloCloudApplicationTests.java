a212a31e13a8eac4ba676ecc97d1c85fb4e73bfc
koodoyoon <kooodoyoon@gmail.com>
2025-06-30 16:17:13 +0900

    init commit

---
+++ src/test/java/com/example/hellocloud/HelloCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
